51896ff764f99313e81917022180dbf383978ed5
Yuko Kitahata <ykitahata@my.bcit.ca>
2022-11-02 15:50:31 -0700

    added product categories on page-items

--- page-items.php
+++ page-items.php
@@ -22,90 +22,49 @@ get_header();
 			the_post();
 			get_template_part( 'template-parts/content', 'page' );
 
-
 			if (function_exists('get_field')){
 
-				if( get_field( 'product_banner_image' )){
+				if( get_field( 'product_banner_image' )) {
 						echo wp_get_attachment_image( get_field( 'product_banner_image' ), 'medium' );
 						$image = get_field('product_banner_image');
 						?>
 						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
 						<?php
 				}
-				if( get_field( 'opening_product_text' )){
+				if( get_field( 'opening_product_text' )) {
 						echo '<p>';	
 						the_field( 'opening_product_text' );
 						echo '</p>';
 				}
 			}
 
-		// Inner Navigation
-		$args = array(
-			'post_type'             => 'items',
-			'post_status'           => 'publish',
-			'ignore_sticky_posts'   => 1,
-			'tax_query'             => array(
-        array(
-            'taxonomy'  => 'product_cat',
-            'field'     => 'term_id',
-            'terms'     => array('40'),
-            'operator'  => 'IN',
-        )
-   		)
-		);
-		$query = new WP_Query($args);
-			if( $query -> have_posts() ){
-				?>
-				<article>
-				<nav class="inner-nav">
-				<?php
-				while ( $query -> have_posts() ){
-					$query -> the_post();
-					$id = get_the_ID();
-					?>
-
-							<a href="#<?php echo $id; ?>"><?php the_title(); ?></a>
-						
-				<?php		
+			// output cagegory names
+			$terms = get_terms( 'product_cat' );
+			if( $terms && ! is_wp_error($terms) ) {
+				foreach($terms as $term){
+					echo '<button>'. $term -> name .'</button>';
 				}
-				wp_reset_postdata();
-				?>
-					</nav>
-					</article>
-					
-				<?php
 			}
-		endwhile; // End of the loop.
+
+			//Products
+			$current_cat_id = $wp_query->get_queried_object()->term_id;
+			$args = array(
+				'post_type'      => 'product',
+				'posts_per_page' => -1,
+			);
+
+			$products = new WP_Query( $args );
+					echo '<section className="section-products">';
+			while ( $products->have_posts() ) : 
+							$products->the_post();
+				echo '<li><div class="product__preview"><img src="' . get_the_post_thumbnail_url() . '"></div><span>' . get_the_title() . '</span></li>';
+			endwhile;
+					echo '</section>';
+
+			wp_reset_postdata();
+
+		endwhile;
 		?>
-		<?php
-
-		//Products
-		$current_cat_id = $wp_query->get_queried_object()->term_id;
-       $args = array(
-         'taxonomy'       => 'product_cat',
-         'post_type'      => 'product',
-         'post_status'    => 'publish',
-         'posts_per_page' => -1,
-         'tax_query'      => array(
-             'taxonomy'   => 'product_cat',
-             'field'      => 'term_id',
-             'terms'      => $current_cat_id,
-             'operator'   => 'IN'
-         )
-       );
-
-			
-
-       $products = new WP_Query( $args );
-			 echo '<section className="section-products">';
-       while ( $products->have_posts() ) : 
-					$products->the_post();
-          echo '<li><div class="product__preview"><img src="' . get_the_post_thumbnail_url() . '"></div><span>' . get_the_title() . '</span></li>';
-       endwhile;
-			 echo '</section>';
-
-       wp_reset_query();
-			 ?>
 
 	</main><!-- #main -->
 
