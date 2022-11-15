c97f31c36c869a7b750c33d2f3601093e4071166
hyeriyoon <109633824+hyeriyoon@users.noreply.github.com>
2022-11-15 14:50:32 -0800

    added admin.php

--- functions.php
+++ functions.php
@@ -222,6 +222,11 @@ add_action('acf/init', 'my_acf_init');
 require get_template_directory() . '/inc/custom-header.php';
 
 /**
+ * admin page
+ */
+require get_template_directory() . '/inc/admin.php';
+
+/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
@@ -289,35 +294,6 @@ function custom_pre_get_posts_query($q)
 
 add_action( 'woocommerce_product_query', 'custom_pre_get_posts_query' );  
 
-// add a custom logo to the login screen - borrowed from https://codex.wordpress.org/Customizing_the_Login_Form
-function my_login_logo() { ?>
-    <style type="text/css">
-        #login h1 a, .login h1 a {
-				background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/tb-logo.svg);
-				height:200px;
-				width:320px;
-				background-size: 320px 200px;
-				background-repeat: no-repeat;
-        	/* padding-bottom: 30px; */
-        }
-    </style>
-<?php }
-add_action( 'login_enqueue_scripts', 'my_login_logo' );
-
-// Change the login admin page to have a link to the Tweeling page and text that says Tweeling Bakery - code borrowed from https://www.wpbeginner.com/plugins/how-to-create-custom-login-page-for-wordpress/
-function tweeling_login_logo_url() {
-    return home_url();
-}
-add_filter( 'login_headerurl', 'tweeling_login_logo_url' );
-  
-function tweeling_login_logo_url_title() {
-    return 'Tweeling Bakery';
-}
-add_filter( 'login_headertitle', 'tweeling_login_logo_url_title' );
-
-
-
-
 //  Remove WordPress Dashboard Widgets - code borrowed from: https://wpbeaches.com/remove-wordpress-backend-dashboard-widgets/
 // and woocommerce setup reminder from https://mainwp.com/how-to-hide-the-setup-dashboard-widget-in-woocommerce/
 
@@ -340,12 +316,6 @@ function tweel_remove_dashboard_widgets() {
 	remove_meta_box( 'wc_admin_dashboard_setup', 'dashboard', 'normal'); // Woocommerce Setup reminder
 }
 add_action( 'wp_dashboard_setup', 'tweel_remove_dashboard_widgets' );
-
-// Remove comments from admin bar borrowed from https://www.isitwp.com/remove-comments-link-from-admin-bar/
-function remove_comments_from_admin_bar() {
-	remove_menu_page( 'edit-comments.php' );
-	}
-	add_action( 'admin_menu', 'remove_comments_from_admin_bar' );
 
 /**
  * Lower Yoast SEO Metabox location
