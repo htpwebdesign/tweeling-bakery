cfa95b8b257b96b72f4e5ce70baa169ed79134a0
Sarah <slowtalon@gmail.com>
2022-11-16 14:45:30 -0800

    testing error correction

--- functions.php
+++ functions.php
@@ -232,7 +232,7 @@ require get_template_directory() . '/inc/custom-header.php';
 /**
  * admin page
  */
-// require get_template_directory() . '/inc/admin.php';
+require get_template_directory() . '/inc/admin.php';
 
 /**
  * Custom template tags for this theme.
@@ -359,20 +359,3 @@ add_action( 'login_enqueue_scripts', 'my_login_logo' );
 
 // stop here
 
-// Change the login admin page to have a link to the Tweeling page and text that says Tweeling Bakery - code borrowed from https://www.wpbeginner.com/plugins/how-to-create-custom-login-page-for-wordpress/
-function tweeling_login_logo_url() {
-  return home_url();
-}
-add_filter( 'login_headerurl', 'tweeling_login_logo_url' );
-
-function tweeling_login_logo_url_title() {
-  return 'Tweeling Bakery';
-}
-add_filter( 'login_headertitle', 'tweeling_login_logo_url_title' );
-
-
-// Remove comments from admin bar borrowed from https://www.isitwp.com/remove-comments-link-from-admin-bar/
-function remove_comments_from_admin_bar() {
-	remove_menu_page( 'edit-comments.php' );
-	}
-	add_action( 'admin_menu', 'remove_comments_from_admin_bar' );
