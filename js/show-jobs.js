57beda2477113df72fde0b7c58a4e15601ff147b
Sarah <slowtalon@gmail.com>
2022-11-15 22:37:06 -0800

    styled 404 page

---
+++ js/show-jobs.js
@@ -0,0 +1,11 @@
+// document.addEventListener("DOMContentLoaded", function (event) {
+// const button = document.querySelectorAll("button-apply-top");
+// const form = document.getElementById("job-form");
+// console.log("Hi!");
+
+// button.addEventListener("click", () => {
+//   console.log("button clicked");
+// document.getElementById("button-apply-top").style.display = "block";
+//   form.style.display = "block";
+// });
+// });
