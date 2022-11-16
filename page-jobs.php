91a9e032ff922362b4d50fab0886950a3f078e79
Joshua Borseth <joshuaborseth@gmail.com>
2022-11-15 16:22:43 -0800

    shows form and scrolls into view on jobs page

--- page-jobs.php
+++ page-jobs.php
@@ -18,46 +18,46 @@ get_header();
 <main id="primary" class="site-main">
 
 	<div class="jobs-wrapper">
-	<?php
-	while (have_posts()) :
-		the_post();
-		the_title( '<h1 class="entry-title">', '</h1>' );
-		echo get_the_post_thumbnail();
-		if (function_exists('get_field')) {
-			if (get_field('careers_opening_blurb')) {
-				echo "<p>";
-				the_field('careers_opening_blurb');
-				echo "</p>";
-			}
-		}
-
-		// If comments are open or we have at least one comment, load up the comment template.
-		if (comments_open() || get_comments_number()) :
-			comments_template();
-		endif;
-
-	endwhile; // End of the loop. end
-	?>
-
-	<section>
-
 		<?php
-		$args = array(
-			'post_type'      => 'tweel-career',
-			'posts_per_page' => -1,
-		);
+		while (have_posts()) :
+			the_post();
+			the_title('<h1 class="entry-title">', '</h1>');
+			echo get_the_post_thumbnail();
+			if (function_exists('get_field')) {
+				if (get_field('careers_opening_blurb')) {
+					echo "<p>";
+					the_field('careers_opening_blurb');
+					echo "</p>";
+				}
+			}
 
-		$query = new WP_Query($args);
+			// If comments are open or we have at least one comment, load up the comment template.
+			if (comments_open() || get_comments_number()) :
+				comments_template();
+			endif;
 
-		if ($query->have_posts()) {
-			while ($query->have_posts()) {
-				$query->the_post();
+		endwhile; // End of the loop. end
 		?>
+
+		<section>
+
+			<?php
+			$args = array(
+				'post_type'      => 'tweel-career',
+				'posts_per_page' => -1,
+			);
+
+			$query = new WP_Query($args);
+
+			if ($query->have_posts()) {
+				while ($query->have_posts()) {
+					$query->the_post();
+			?>
 					<div class="title-apply">
 						<h2><?php echo the_title(); ?></h2>
 						<button class="button-apply-top">Apply now</button>
 					</div>
-					<?php
+			<?php
 					$term = (get_the_terms(get_the_ID(), 'tweel-career-locations'));
 					if ($term) {
 						echo "<section class='job-section'>";
@@ -80,21 +80,20 @@ get_header();
 							the_field('wages-hourly');
 							echo " per hour</p>";
 						}
-						
+
 						echo "<button class='button-apply-bottom'>Apply now</button>";
 						echo "</article>";
 						echo "</section>";
 					}
-						
-						}
-						echo "<section id='job-form'>";
-						gravity_form( 2, false, false, false, '', false );
-						wp_reset_postdata();
-						echo "</section>";
-					}
-							?>
+				}
+				echo "<section id='job-form'>";
+				gravity_form(2, false, false, false, '', false);
+				wp_reset_postdata();
+				echo "</section>";
+			}
+			?>
 
-	</section>
+		</section>
 
 	</div>
 </main><!-- #main -->
